3eaffe50904543ccd2d45e2def83003cab432d47
eaglewu <eaglewudi@gmail.com>
2016-02-22 14:30:38 +0800

    Add comments for class Timer

--- src/Timer.php
+++ src/Timer.php
@@ -18,10 +18,13 @@ namespace Swoole;
 class Timer
 {
     /**
-     * @param int $ms
-     * @param callable $callback
+     * 设置一个间隔时钟定时器，与after定时器不同的是tick定时器会持续触发，直到调用swoole_timer_clear清除。与swoole_timer_add不同的是tick定时器可以存在多个相同间隔时间的定时器。
+     *
+     * @param int $ms 指定时间，单位为毫秒
+     * @param callable $callback 时间到期后所执行的函数，必须是可以调用的。callback函数不接受任何参数
+     * @param mixed $param 回调参数
      */
-    static function tick($ms, callable $callback)
+    static function tick($ms, callable $callback, $param = null)
     {
 
     }
