64b756070ea65fa38e62a1763e950aa72cdc24fa
wangj <wangj@2345.com>
2018-06-28 10:37:45 +0800

    增加Channel的函数列表

---
+++ src/Coroutine/Channel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Swoole\Coroutine;
+
+class Channel
+{
+    public static function select(&$read_list, &$write_list, $timeout)
+    {
+    }
+
+    public $capacity;
+
+    public function __construct($size)
+    {
+
+    }
+
+    public function pop()
+    {
+    }
+
+    public function isEmpty()
+    {
+    }
+
+    public function isFull()
+    {
+    }
+
+    public function close()
+    {
+    }
+
+    public function stats()
+    {
+    }
+
+    public function length()
+    {
+    }
+}
+
+# end of file
