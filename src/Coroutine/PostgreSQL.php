272ee37633109bebd98b05f367fffd160a8d8d8f
eagle <eaglewudi@gmail.com>
2018-10-10 11:34:50 +0800

    Add Coroutine\PostgreSQL & Coroutine\Runtime

---
+++ src/Coroutine/PostgreSQL.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * swoole-ide-helper
+ *
+ * Author: eaglewu <eaglewudi@gmail.com>
+ * Datetime: 2018/10/10 10:49
+ */
+
+namespace Swoole\Coroutine;
+
+
+class PostgreSQL
+{
+
+    /**
+     * 建立postgresql 非阻塞的协程连接
+     *
+     * @link https://wiki.swoole.com/wiki/page/884.html
+     * @param string $connection_string
+     * @return resource
+     */
+    public function connect(string $connection_string)
+    {
+
+    }
+
+    /**
+     * 发送异步非阻塞 协程命令
+     *
+     * @param resource $connection
+     * @return $this
+     */
+    public function query($connection)
+    {
+
+    }
+
+    /**
+     * 提取结果中所有行作为一个数组
+     *
+     * @link https://wiki.swoole.com/wiki/page/886.html
+     * @param resource $query
+     * @return array
+     */
+    public function fetchAll($query)
+    {
+
+    }
+
+    /**
+     * 返回受影响的记录数目
+     *
+     * @link https://wiki.swoole.com/wiki/page/887.html
+     * @param resource $queryResult
+     * @return int
+     */
+    public function affectedRows($queryResult)
+    {
+
+    }
+
+    /**
+     * 返回行的数目
+     *
+     * @link https://wiki.swoole.com/wiki/page/888.html
+     * @param resource $queryResult
+     * @return int
+     */
+    public function numRows($queryResult)
+    {
+
+    }
+
+    /**
+     * 提取一行作为对象
+     *
+     * @link https://wiki.swoole.com/wiki/page/889.html
+     * @param resource $queryResult
+     * @param int $row
+     * @return object
+     */
+    public function fetchObject($queryResult, $row = 0)
+    {
+
+    }
+
+    /**
+     * 提取一行作为关联数组
+     *
+     * @link https://wiki.swoole.com/wiki/page/890.html
+     * @param resource $queryResult
+     * @param int $row
+     * @return array
+     */
+    public function fetchAssoc($queryResult, $row = 0)
+    {
+
+    }
+
+    /**
+     * 提取一行作为数组
+     *
+     * @link https://wiki.swoole.com/wiki/page/891.html
+     * @param resource $queryResult
+     * @param int $row
+     * @return array
+     */
+    public function fetchArray($queryResult, $row = 0)
+    {
+
+    }
+
+    /**
+     * 根据指定的 result 资源提取一行数据（记录）作为数组返回
+     *
+     * @link https://wiki.swoole.com/wiki/page/892.html
+     * @param resource $queryResult
+     * @param int $row
+     * @return array
+     */
+    public function fetchRow($queryResult, $row = 0)
+    {
+
+    }
+
+    /**
+     * 查看表的元数据 异步非阻塞协程版
+     *
+     * @link https://wiki.swoole.com/wiki/page/893.html
+     * @param string $tableName
+     * @return array
+     */
+    public function metaData($tableName)
+    {
+
+    }
+
+}
---
+++ src/Coroutine/Runtime.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * swoole-ide-helper
+ *
+ * Author: eaglewu <eaglewudi@gmail.com>
+ * Datetime: 2018/10/10 10:47
+ */
+
+namespace Swoole\Coroutine;
+
+
+class Runtime
+{
+
+    /**
+     * 运行时动态将基于php_stream实现的扩展、PHP网络客户端代码一键协程化
+     *
+     * @since 4.1.0
+     *
+     * 4.1 版本仅支持tcp和 unix两种stream类型
+     * 4.2 版本增加了对 udp、udg、unix、ssl、tls 类型的支持
+     *
+     * @link https://wiki.swoole.com/wiki/page/965.html
+     * @param bool $enable
+     * @param int $flags
+     */
+    public static function enableCoroutine($enable = true, $flags = SWOOLE_HOOK_ALL)
+    {
+
+    }
+
+}
