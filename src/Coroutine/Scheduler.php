f654bc4b7246989b546d7f9654b910dc46146e76
AlexFu <ryoalex@foxmail.com>
2020-07-05 00:41:05 +0800

    增加定时器相关

---
+++ src/Coroutine/Scheduler.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Swoole\Coroutine;
+
+
+/**
+ * 协程定时器
+ * Class Scheduler
+ * @package Swoole\Coroutine
+ */
+class Scheduler
+{
+    /**
+     * 增加一个定时器
+     * @param callable $func
+     * @param $params
+     */
+    public static function add(callable $func, $params): void{
+
+    }
+
+
+    public static function parallel(callable $func, $params){
+
+    }
+
+    /**
+     * @return bool
+     */
+    public static function start(){
+
+    }
+
+
+    public function set(array $settings){
+
+    }
+}
